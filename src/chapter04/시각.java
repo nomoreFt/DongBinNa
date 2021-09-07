21be17f541b7f697307164e0b587a2688238a477
nomoreFt <hyunwoo.kim1108@gmail.com>
2021-09-07 10:46:07 +0900

    done

---
+++ src/chapter04/시각.java
@@ -0,0 +1,24 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class ½Ã°¢ {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int cnt = 0;
+		for(int i = 0 ; i <= N; i++) {
+			for(int j = 0; j <= 59; j++ ) {
+				for(int k = 0; k <= 59; k++) {
+					String str = Integer.toString(i)+Integer.toString(j)+Integer.toString(k);
+					if(str.contains("3")) {
+						cnt++;
+					}
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+}
