3722cbe01984c781a2b74f30c5189b628eb765f6
nomoreFt <hyunwoo.kim1108@gmail.com>
2022-02-12 17:20:33 +0900

    add Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    static final int INF = (int) 1e9;
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = INF;
+        int temp = n;
         
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(sc.nextInt());
-        arr.add(sc.nextInt());
+        int max5 = 0;
+        max5 = n / 5;
+        if(n == 3) {
+            System.out.println("1");
+            return;
+        }
+
+        if(n == 4){
+            System.out.println("-1");
+            return;
+        }
+        for(int i = max5; i >= 0; i--){
+            temp = n;
+            temp -= 5 * max5;
+            if(temp == 0){
+                result = Math.min(result,i);
+            }else{
+                if(temp % 3 == 0){
+                   result = Math.min(result,(i + temp/3)); 
+                }else{
+                    continue;
+                }
+            }
+        }
+        if(result >= INF){
+            System.out.println("-1");
+        }else{
+            System.out.println(result);
+        }
         
-        System.out.println(arr.stream().mapToInt(i -> i).sum());
-    }
+}
 }
