d34e291e46e42e999c90d839838d0338b93917cc
nomoreFt <hyunwoo.kim1108@gmail.com>
2022-02-13 16:02:29 +0900

    add

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,44 @@
 import java.util.*;
 
 public class Main {
-    static final int INF = (int) 1e9;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int result = INF;
-        int temp = n;
-        
-        int max5 = 0;
-        max5 = n / 5;
-        if(n == 3) {
-            System.out.println("1");
-            return;
-        }
+       
+        int T = sc.nextInt();
+        int cnt300 = 0;
+        int cnt60 = 0;
+        int cnt10 = 0;
 
-        if(n == 4){
-            System.out.println("-1");
-            return;
-        }
-        for(int i = max5; i >= 0; i--){
-            temp = n;
-            temp -= 5 * max5;
-            if(temp == 0){
-                result = Math.min(result,i);
-            }else{
-                if(temp % 3 == 0){
-                   result = Math.min(result,(i + temp/3)); 
-                }else{
-                    continue;
-                }
+        if(T <300){
+            while(T % 60 > 0){
+                T -= 10;
+                cnt10++;
+            }
+            if(T < 0) {
+                System.out.println("-1");
+                return;
+            }
+            cnt60 += T/60;
+        }else{
+            while(T >= 300){
+                T-=300;
+                cnt300++;
+            }
+            while(T >= 60){
+                T-=60;
+                cnt60++;
+            }
+            while(T >= 10){
+                T-=10;
+                cnt10++;
+            }
+            if(T <= 9 && T != 0){
+                System.out.println("-1");
+                return;
             }
         }
-        if(result >= INF){
-            System.out.println("-1");
-        }else{
-            System.out.println(result);
-        }
-        
+
+        System.out.println(cnt300 + " " + cnt60 + " " + cnt10);
+      
 }
 }
